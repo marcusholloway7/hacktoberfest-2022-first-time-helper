6bb003b097dcb6b9ef9a0120ce3632c60ad1077e
Jernickjoshi001 <117035899+Jernickjoshi001@users.noreply.github.com>
2022-10-30 23:41:51 +0530

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,9 @@
-#to check whether a number is palindrome(pali.py)
-num=int(input("Enter the number:"))
-d=r=0
-temp=num
-while(temp!=0):
-    d=temp%10
-    r=(r*10)+d
+n=int(input())
+temp=n;rev=0
+while(temp):
+    rev=rev*10+temp%10
     temp=temp//10
-if (r==num):
-    print(num,"is a palindrome")
+if(n==rev):
+    print("palindrome")
 else:
-    print(num,"is not a palindrome")
-    
-    
-   
+    print("not palindrome")
